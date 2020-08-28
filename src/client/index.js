8204bce71c2fa87bd72c3ae85ed6624ded8fdbb0
Maimounah <62860815+Maimounah97@users.noreply.github.com>
2020-08-28 04:25:05 +0300

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,20 @@
+
+import { handleSubmit } from './js/formHandler'
+import { getCityCoordinates } from './js/formHandler'
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
+
+
+
+alert("I EXIST")
+
+export {
+   
+    handleSubmit,
+    getCityCoordinates
+}
+
